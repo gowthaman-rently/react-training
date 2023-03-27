94ca5ebb290bc5a9a69055e2aa719a7016a29de5
gowthaman-rently <gowthaman.ks@rently.com>
2023-03-27 18:05:12 +0530

    connector update

--- github/src/components/connector.js
+++ github/src/components/connector.js
@@ -1,13 +1,12 @@
 import axios from "axios";
 
-async function connect(method, url){
+const  connect = async(method, url)=>{
     try{
-        return await axios.request(url,{method:method, "headers" : {"Authorization": "Bearer  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", 'accept': 'application/vnd.github.VERSION.raw'}});
+        return await axios.request(url,{method:method});
     }
     catch(error){
         return error;
-    }
-    
+    }    
 }   
 
 export default connect;
